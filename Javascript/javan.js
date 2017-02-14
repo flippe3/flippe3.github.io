79a05f93c9828049a608c86575e291f1b90285d8
flippe3 <filip.c.nilsson@gmail.com>
2017-02-14 10:42:36 +0100

    Update javan.js

--- Javascript/javan.js
+++ Javascript/javan.js
@@ -2,7 +2,7 @@
 function myFunction() {
     var x = document.getElementById("myNumber").value;
     
-	if(isNaN(x))
+	if(isNaN(x) || x == "")
 	{
 		alert("NOT A NUMBER");
 		document.getElementById("para").innerHTML = "You did not enter a number";
