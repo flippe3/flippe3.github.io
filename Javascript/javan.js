54a436c8b6020b68b627f84e28ad3f863eb16c71
flippe3 <filip.c.nilsson@gmail.com>
2017-02-14 10:37:31 +0100

    Create javan.js

---
+++ Javascript/javan.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function myFunction() {
+    var x = document.getElementById("myNumber").value;
+    
+	if(isNaN(x))
+	{
+		alert("NOT A NUMBER");
+		document.getElementById("para").innerHTML = "You did not enter a number";
+	}
+	else
+	{
+		document.getElementById("para").innerHTML = "You wrote: " + x;
+	}
+}
