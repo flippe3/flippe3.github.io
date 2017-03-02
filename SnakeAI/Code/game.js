2ff4a4edac757c5d0c5cc2a3d565056d27867f8b
flippe3 <filip.c.nilsson@gmail.com>
2017-03-02 19:46:40 +0100

    Add files via upload

--- SnakeAI/Code/game.js
+++ SnakeAI/Code/game.js
@@ -8,53 +8,56 @@ var rows = 30;
 
 function setup()
 {
-	frameRate(40);
-	var myCanvas = createCanvas(600, 600);
-	myCanvas.parent('myContainer');
-	snake = new Snake();
-	document.getElementById("snake_length").innerHTML = "Current length: " + snake_length;
-	gen_food();
+    frameRate(40);
+    var myCanvas = createCanvas(600, 600);
+    myCanvas.parent('myContainer');
+    snake = new Snake();
+    document.getElementById("snake_length").innerHTML = "Current length: " + snake_length;
+    gen_food();
 }
 function draw()
 {
-	background(0);
-	snake.update();
-	snake.show();
-	simple_ai();
-	dodge();
-	check_gameover();
-	console.log("snakeX" + snakeX[0]);
-	console.log("snakeY" + snakeY[0]);
+    background(0);
+    simple_ai();
+    dodge();
+    avoid_walls();
+    check_gameover();
+    snake.update();
+    snake.show();
+    //console.log("snakeX" + snakeX[0]);
+    //console.log("snakeY" + snakeY[0]);
+    document.getElementById("xpos").innerHTML = "up: " + check_up();
+    document.getElementById("ypos").innerHTML = "down: " + check_down();
 }
 function keyPressed()
 {
-	if(keyCode === UP_ARROW && snake_dir != -1)
-	{
-		snake_dir = 1;
-		snake.dir(0, -1);
-	}	
-	else if(keyCode === DOWN_ARROW && snake_dir != 1)
-	{
-		snake_dir = -1;
-		snake.dir(0, 1);
-	}
-	else if(keyCode === RIGHT_ARROW && snake_dir != -2)
-	{
-		snake_dir = 2;
-		snake.dir(1, 0);
-	}
-	else if(keyCode === LEFT_ARROW && snake_dir != 2)
-	{
-		snake_dir = -2;
-		snake.dir(-1, 0);
-	}
-	else if(keyCode === 81)
-		snake_length++;
+    if(keyCode === UP_ARROW && snake_dir != -1)
+    {
+	snake_dir = 1;
+	snake.dir(0, -1);
+    }	
+    else if(keyCode === DOWN_ARROW && snake_dir != 1)
+    {
+	snake_dir = -1;
+	snake.dir(0, 1);
+    }
+    else if(keyCode === RIGHT_ARROW && snake_dir != -2)
+    {
+	snake_dir = 2;
+	snake.dir(1, 0);
+    }
+    else if(keyCode === LEFT_ARROW && snake_dir != 2)
+    {
+	snake_dir = -2;
+	snake.dir(-1, 0);
+    }
+    else if(keyCode === 81)
+	snake_length++;
 }
 function gen_food()
 {
-	var cls = floor(28);
-	var rws = floor(28);
-	foodX = (floor(random(cls)) * 20) + 20;
-	foodY = (floor(random(rws)) * 20) + 20;
+    var cls = floor(28);
+    var rws = floor(28);
+    foodX = (floor(random(cls)) * 20) + 20;
+    foodY = (floor(random(rws)) * 20) + 20;
 }
--- SnakeAI/Code/solo_ai.js
+++ SnakeAI/Code/solo_ai.js
@@ -4,145 +4,208 @@
 //snake_dir -1 == up
 //snake_dir 2 == right
 //snake_dir -2 == left
+var crashed = false;
 function simple_ai()
 {
-	if(foodY < snakeY[0] && snake_dir != 1)
-	{
-		snake_dir = -1;
-		snake.dir(0, -1);
-	}	
-	else if(foodY > snakeY[0] && snake_dir != -1)
-	{
-		snake_dir = 1;
-		snake.dir(0, 1);
-	}
-	else if(foodX > snakeX[0] && snake_dir != -2)
-	{
-		snake_dir = 2;
-		snake.dir(1, 0);
-	}
-	else if(foodX < snakeX[0] && snake_dir != 2)
-	{
-		snake_dir = -2;
-		snake.dir(-1, 0);
-	}
+    if(foodY < snakeY[0] && snake_dir != 1 && !check_up())
+    {
+	snake_dir = -1;
+	snake.dir(0, -1);
+    }	
+    else if(foodY > snakeY[0] && snake_dir != -1 && !check_down())
+    {
+	snake_dir = 1;
+	snake.dir(0, 1);
+    }
+    else if(foodX > snakeX[0] && snake_dir != -2 && !check_right())
+    {
+	snake_dir = 2;
+	snake.dir(1, 0);
+    }
+    else if(foodX < snakeX[0] && snake_dir != 2 && !check_left())
+    {
+	snake_dir = -2;
+	snake.dir(-1, 0);
+    }
 }
 function dodge()
 {
-	for(var i = 0; i < snake_length; i++)
+    crashed = false;
+    for(var i = 0; i < snake_length; i++)
+    {
+	if(snake_dir === 1 && check_down())
 	{
-		if(snake_dir === 1  && snakeY[0] + 20 === snakeY[i] && snakeX[0] === snakeX[i])
-		{
-			if(!check_left())
-			{
-				snake_dir = -2
-				snake.dir(-1, 0);
-			}
-			else if(!check_right())
-			{
-				snake_dir = 2;
-				snake.dir(1, 0);
-			}
-		}
-		else if(snake_dir === -1  && snakeY[0] - 20 === snakeY[i] && snakeX[0] === snakeX[i])
-		{
-			if(!check_left())
-			{
-				snake_dir = -2
-				snake.dir(-1, 0);
-			}
-			else if(!check_right())
-			{
-				snake_dir = 2;
-				snake.dir(1, 0);
-			}
-		}
-		else if(snake_dir === -2 && snakeY[0] === snakeY[i] && snakeX[0] - 20 === snakeX[i])
-		{
-			if(!check_up())
-			{
-				snake_dir = 1;
-				snake.dir(0, -1);
-			}
-			else if(!check_down())
-			{
-				snake_dir = -1;
-				snake.dir(0, 1);
-			}
-		}
-		else if(snake_dir === 2 && snakeY[0] === snakeY[i] && snakeX[0] + 20 === snakeX[i])
-		{
-			if(!check_up())
-			{
-				snake_dir = 1;
-				snake.dir(0, -1);
-			}
-			else if(!check_down())
-			{
-				snake_dir = -1;
-				snake.dir(0, 1);
-			}
-		}
+            crashed = true;
+	    if(!check_left())
+	    {
+		snake_dir = -2;
+		snake.dir(-1, 0);
+                console.log("Going left");
+	    }
+	    else if(!check_right())
+	    {
+		snake_dir = 2;
+		snake.dir(1, 0);
+                console.log("Going right");
+	    }
 	}
+	else if(snake_dir === -1 && check_up())
+	{
+            crashed = true;
+	    if(!check_left())
+	    {
+		snake_dir = -2;
+		snake.dir(-1, 0);
+                console.log("Going left");
+	    }
+	    else if(!check_right())
+	    {
+		snake_dir = 2;
+		snake.dir(1, 0);
+                console.log("Going right");
+	    }
+	}
+	else if(snake_dir === -2 && check_left())
+	{
+            crashed = true;
+	    if(!check_up())
+	    {
+		snake_dir = -1;
+		snake.dir(0, -1);
+                console.log("Going up");
+	    }
+	    else if(!check_down())
+	    {
+		snake_dir = 1;
+		snake.dir(0, 1);
+                console.log("Going down");
+	    }
+	}
+	else if(snake_dir === 2 && check_right())
+	{
+            crashed = true;
+	    if(!check_up())
+	    {
+		snake_dir = -1;
+		snake.dir(0, -1);
+                console.log("Going up");
+	    }
+	    else if(!check_down())
+	    {
+		snake_dir = 1;
+		snake.dir(0, 1);
+                console.log("Going down");
+	    }
+	}
+    }
 }
+
+function avoid_walls()
+{
+    if(snake_dir === -1 && snakeY[0] - 20 === 0 && crashed === false)
+    {
+        if(!check_left())
+	{
+	    snake_dir = -2;
+	    snake.dir(-1, 0);
+            console.log("Going left");
+	}
+	else if(!check_right())
+	{
+	    snake_dir = 2;
+	    snake.dir(1, 0);
+            console.log("Going right");
+	}
+    }
+    else if(snake_dir === 1 && snakeY[0] + 20 === 580 && crashed === false)
+    {
+        if(!check_left())
+	{
+	    snake_dir = -2;
+	    snake.dir(-1, 0);
+	}
+	else if(!check_right())
+	{
+	    snake_dir = 2;
+	    snake.dir(1, 0);
+	}
+    }
+    else if(snake_dir === 2 && snakeX[0] + 20 === 580 && crashed === false)
+    {
+        if(!check_up())
+	{
+	    snake_dir = -1;
+	    snake.dir(0, -1);
+	}
+	else if(!check_down())
+	{
+	    snake_dir = 1;
+	    snake.dir(0, 1);
+	}
+    }
+    else if(snake_dir === -2 && snakeX[0] - 20 === 0 && crashed === false)
+    {
+        if(!check_up())
+	{
+	    snake_dir = -1;
+	    snake.dir(0, -1);
+	}
+	else if(!check_down())
+	{
+	    snake_dir = 1;
+	    snake.dir(0, 1);
+	}
+    }
+}
+
 //snake_dir -1 == down
 //snake_dir 1 == up
 //snake_dir 2 == right
 //snake_dir -2 == left
 function check_left()
 {
-	for(var i = 0; i < snake_length; i++)
-	{
-		if(snakeY[i] === snakeY[0] && snakeX[i] === snakeX[0] - 20)
-			return true;
-	}
-	if(snakeX[0] - 20 === 0)
-		return true;
-	return false;
+    for(var i = 0; i < snake_length; i++)
+    {
+	if(snakeY[i] === snakeY[0] && snakeX[i] === snakeX[0] - 20)
+	    return true;
+    }
+    return false;
 }
 function check_right()
 {
-	for(var i = 0; i < snake_length; i++)
-	{
-		if(snakeY[i] === snakeY[0] && snakeX[i] === snakeX[0] + 20)
-			return true;
-	}
-	if(snakeX[0] + 20 === 580)
-		return true;
-	
-	return false;
+    for(var i = 0; i < snake_length; i++)
+    {
+	if(snakeY[i] === snakeY[0] && snakeX[i] === snakeX[0] + 20)
+	    return true;
+    }
+    return false;
 }
 function check_up()
 {
-	for(var i = 0; i < snake_length; i++)
-	{
-		if(snakeY[i] === snakeY[0] - 20 && snakeX[i] === snakeX[0])
-			return true;
-	}
-	if(snakeY[0] - 20 === 0)
-		return true;
-	
-	return false;
+    for(var i = 0; i < snake_length; i++)
+    {
+	if(snakeY[i] === snakeY[0] - 20 && snakeX[i] === snakeX[0])
+	    return true;
+    }
+    return false;
 }
 function check_down()
 {
-	for(var i = 0; i < snake_length; i++)
-	{
-		if(snakeY[i] === snakeY[0] + 20 && snakeX[i] === snakeX[0])
-			return true;
-	}
-	if(snakeY[0] + 20 === 580)
-		return true;
-	return false;
+    for(var i = 0; i < snake_length; i++)
+    {
+	if(snakeY[i] === snakeY[0] + 20 && snakeX[i] === snakeX[0])
+	    return true;
+    }
+    return false;
 }
 function check_gameover()
 {
-	if(snakeX[0] === 0 || snakeX[0] === 560 || snakeY[0] === 0 || snakeY[0] === 0)
-		gameOver = true;
-	
-	for(var i = 1; i < snake_length; i++)
-	{
-		if(snakeX[0] === snakeX[i] && snakeY[0] === snakeY[i])
-			gameOver = true;
-	}
+    if(snakeX[0] === 0 || snakeX[0] === 580 || snakeY[0] === 0 || snakeY[0] === 580)
+	gameOver = true;
+    
+    for(var i = 1; i < snake_length; i++)
+    {
+	if(snakeX[0] === snakeX[i] && snakeY[0] === snakeY[i])
+	    gameOver = true;
+    }
 }
